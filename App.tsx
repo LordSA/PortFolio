bc881e07e5741344911673f95dee6a1d703055aa
LordSA <96365910+LordSA@users.noreply.github.com>
2025-08-31 20:50:59 +0530

    base

---
+++ App.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Navigation } from './components/Navigation'
+import { HeroSection } from './components/HeroSection'
+import { AboutSection } from './components/AboutSection'
+import { ExperienceSection } from './components/ExperienceSection'
+import { SkillsSection } from './components/SkillsSection'
+import { ProjectsSection } from './components/ProjectsSection'
+import { ContactSection } from './components/ContactSection'
+import { Footer } from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground antialiased">
+      <Navigation />
+      <main className="relative">
+        <HeroSection />
+        <AboutSection />
+        <ExperienceSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
