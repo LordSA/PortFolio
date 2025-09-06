f17f522bbbdf9b4eabd3a06c96f876b1c06cc8b6
LordSA <96365910+LordSA@users.noreply.github.com>
2025-09-07 00:56:55 +0530

    Update ContactSection.tsx

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'motion/react'
+import { motion } from "framer-motion";
 import { useInView } from './hooks/useInView'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
